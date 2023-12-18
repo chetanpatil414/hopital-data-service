99100bfda52e64c63849aad92c6b686c7102fa43
chetanpatil414 <chetanpatil414@gmail.com>
2023-12-18 10:48:42 +0530

    intial change

---
+++ hospital-data-service/src/main/java/com/hospital/HospitalDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalDataServiceApplication.class, args);
+	}
+
+}
